8034c3eb71733b1d9dda2cb79129ba35d27037fd
alsry922 <alsry922@gmail.com>
2021-08-25 14:28:15 +0900

    modify show popular video

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import VideoItem from "../video_item/video_item";
+import styles from "./video_list.module.css";
 
 const VideoList = (props) => {
-  console.log(props);
   return (
-    <ul className="video-list">
+    <ul className={styles.videoList}>
       {props.videos.map((video) => {
         return <VideoItem key={video.id} video={video}></VideoItem>;
       })}
