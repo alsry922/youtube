09b7751bccb091373213a68b41a111e771dab15b
alsry922 <alsry922@gmail.com>
2021-08-27 23:46:33 +0900

    add youtube service

---
+++ src/service/youtube.js
@@ -0,0 +1,31 @@
+export default class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+  }
+
+  async mostPopular() {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos/?part=snippet%2CcontentDetails%2Cstatistics&maxResults=25&chart=mostPopular&regionCode=KR&key=${this.key}`,
+      this.requestOptions
+    );
+    const result_1 = await response.json();
+    return result_1.items;
+  }
+
+  async search(query) {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+      this.requestOptions
+    );
+    const result_1 = await response.json();
+    const items = result_1.items.map((item) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
+    return items;
+  }
+}
